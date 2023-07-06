2591e97d6bb7a936744855cd801d090a1c19a2b9
SaidaOuachhal <saidaouachhal1999@gmail.com>
2023-07-06 13:37:41 +0200

    modification index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+{/*import React from 'react';
 <<<<<<< HEAD
 =======
 <<<<<<< HEAD
@@ -37,3 +37,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+
